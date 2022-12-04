fd060abce0f480c4a76284520d067d5d9ce5a65a
Matthew Brown <matthew.brown@genomicsengland.co.uk>
2022-12-04 11:59:08 +0000

    Add cmd args to main

--- 2022/aoc.go
+++ 2022/aoc.go
@@ -4,12 +4,33 @@ import (
 	"aoc/day1"
 	"aoc/day2"
 	"aoc/day3"
+	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
-	testmode := false
+	var testmode bool
+	var day int
 
-	day1.Main(testmode)
+	flag.IntVar(&day, "day", 0, "Run day n scenario")
+	flag.BoolVar(&testmode, "test", false, "Activate test mode")
+	flag.Parse()
+
+	fmt.Printf("Test mode: %t\n", testmode)
+
+	switch day {
+	case 1:
+		day1.Main(testmode)
+	case 2:
+		day2.Main(testmode)
+	case 3:
+		day3.Main(testmode)
+	default:
+		fmt.Println("Invalid day scenario selected")
+		os.Exit(1)
+	}
+
 	day2.Main(testmode)
 	day3.Main(testmode)
 }
