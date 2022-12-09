f4b063f960fa9cf2199ee7e6657df88f2d6979f2
Matthew Brown <matthew.brown@genomicsengland.co.uk>
2022-12-09 19:19:36 +0000

    Day 9 part 2

--- 2022/day9/day9.go
+++ 2022/day9/day9.go
@@ -3,7 +3,6 @@ package day9
 import (
 	"aoc/util"
 	"fmt"
-
 	"strconv"
 )
 
@@ -16,11 +15,49 @@ type Position struct {
 	y int
 }
 
+type VisitedPositions map[Position]int
+
 func Abs(x int) int {
 	if x < 0 {
 		return -x
 	}
 	return x
+}
+
+func Print(positions []Position) {
+	// Find max/min coords
+	var xMax int
+	var yMax int
+	var xMin int
+	var yMin int
+	for _, p := range positions {
+		if p.x > xMax {
+			xMax = p.x
+		}
+		if p.x < xMin {
+			xMin = p.x
+		}
+		if p.y > yMax {
+			yMax = p.y
+		}
+		if p.y < yMin {
+			yMin = p.y
+		}
+	}
+	for y := yMin; y <= yMax; y++ {
+		for x := xMin; x <= xMax; x++ {
+			z := Position{x, y}
+			t := "."
+			for i, p := range positions {
+				if z == p {
+					t = strconv.Itoa(i)
+					break
+				}
+			}
+			fmt.Printf(" %s ", t)
+		}
+		fmt.Println("")
+	}
 }
 
 func readMoves(input *[][]string) []Move {
@@ -32,10 +69,82 @@ func readMoves(input *[][]string) []Move {
 	return moves
 }
 
+func moveHead(m Move, head *Position) {
+	switch m.direction {
+	case "R":
+		head.x++
+	case "L":
+		head.x--
+	case "U":
+		head.y++
+	case "D":
+		head.y--
+	}
+}
+
+func moveRope(head, tail *Position) *Position {
+	// Does the tail have to move?
+	if Abs(head.x-tail.x) > 1 || Abs(head.y-tail.y) > 1 {
+		// If gap is across row AND column, then the tail moves diagonally
+		if (head.x-tail.x) != 0 && (head.y-tail.y) != 0 {
+			if head.x-tail.x > 0 {
+				tail.x++
+			} else {
+				tail.x--
+			}
+			if head.y-tail.y > 0 {
+				tail.y++
+			} else {
+				tail.y--
+			}
+		} else {
+			// normal horiz or vertical move
+			if head.x-tail.x == 0 {
+				if head.y-tail.y > 0 {
+					tail.y++
+				} else {
+					tail.y--
+				}
+			} else {
+				if head.x-tail.x > 0 {
+					tail.x++
+				} else {
+					tail.x--
+				}
+			}
+		}
+		return tail
+	}
+	return nil
+}
+
+func whipRope(moves []Move, knots []Position) int {
+	tailVisited := VisitedPositions{knots[0]: 1} // We know the tail starts at same pos as head
+	for _, m := range moves {
+		// fmt.Println(m)
+		for s := 0; s < m.steps; s++ {
+			moveHead(m, &knots[0])
+			for i := 0; i < len(knots)-1; i++ {
+				tailMoved := moveRope(&knots[i], &knots[i+1])
+				if tailMoved == nil {
+					break
+				}
+				// Track true end of the tail
+				if i+1 == len(knots)-1 {
+					tailVisited[*tailMoved] = 1
+				}
+			}
+		}
+	}
+	// fmt.Println(tailVisited)
+	return len(tailVisited)
+}
+
 func Main(testmode bool) {
-	var input [][]string
+	var input1 [][]string
+	var input2 [][]string
 	if testmode {
-		input = [][]string{
+		input1 = [][]string{
 			{"R", "4"},
 			{"U", "4"},
 			{"L", "3"},
@@ -45,40 +154,32 @@ func Main(testmode bool) {
 			{"L", "5"},
 			{"R", "2"},
 		}
-	} else {
-		input = util.ReadInput("day9/day9.txt", " ").Tokens
-	}
-
-	moves := readMoves(&input)
-	head := Position{0, 0}
-	tail := Position{0, 0}
-
-	tailVisited := map[Position]int{tail: 1}
-
-	for _, m := range moves {
-		fmt.Print(m)
-		for s := 0; s < m.steps; s++ {
-			initialHeadPosition := head
-			switch m.direction {
-			case "R":
-				head.x++
-			case "L":
-				head.x--
-			case "U":
-				head.y++
-			case "D":
-				head.y--
-			}
-
-			// Does the tail have to move?
-			if Abs(head.x-tail.x) > 1 || Abs(head.y-tail.y) > 1 {
-				tail = initialHeadPosition
-				tailVisited[tail] = 1
-			}
-			fmt.Print(head, tail)
+		input2 = [][]string{
+			{"R", "5"},
+			{"U", "8"},
+			{"L", "8"},
+			{"D", "3"},
+			{"R", "17"},
+			{"D", "10"},
+			{"L", "25"},
+			{"U", "20"},
 		}
-		fmt.Println("--------")
+	} else {
+		input1 = util.ReadInput("day9/day9.txt", " ").Tokens
+		input2 = input1
 	}
 
-	fmt.Printf("Tail visited %d positions\n", len(tailVisited))
+	// Part 1
+	moves := readMoves(&input1)
+	knots := make([]Position, 2)
+	tailVisited := whipRope(moves, knots)
+	// Print(knots)
+	fmt.Printf("Tail visited %d positions\n", tailVisited)
+
+	// Part 2
+	moves = readMoves(&input2)
+	knots = make([]Position, 10)
+	tailVisited = whipRope(moves, knots)
+	// Print(knots)
+	fmt.Printf("Tail visited %d positions\n", tailVisited)
 }
