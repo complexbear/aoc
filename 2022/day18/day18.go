a0aa3f1307fafc1237d8f38abc803cd89ebbc0d8
Matt Brown <matthewpeterbrown@gmail.com>
2022-12-29 10:02:43 +0000

    Day 18 - part 2 - floodFill

--- 2022/day18/day18.go
+++ 2022/day18/day18.go
@@ -6,26 +6,22 @@ import (
 	"strconv"
 )
 
-type DropletMap map[Droplet]int
-
-var verticies DropletMap = make(DropletMap, 0)
-var interiorDroplets DropletMap = make(DropletMap, 0)
-
-var minX DropletMap = DropletMap{}
-var maxX DropletMap = DropletMap{}
-var minY DropletMap = DropletMap{}
-var maxY DropletMap = DropletMap{}
-var minZ DropletMap = DropletMap{}
-var maxZ DropletMap = DropletMap{}
-
 type Droplet struct {
 	x int
 	y int
 	z int
 }
+type DropletMap map[Droplet]int
 
-func (d Droplet) Area() int {
-	return (2 * d.x) + (2 * d.y) + (2 * d.z)
+var verticies DropletMap = make(DropletMap, 0)
+var minDroplet Droplet = Droplet{10000, 10000, 10000}
+var maxDroplet Droplet = Droplet{-10000, -10000, -10000}
+
+func cubeArea(d1, d2 Droplet) int {
+	dx := d2.x-d1.x 
+	dy := d2.y-d1.y
+	dz := d2.z-d1.z
+	return (dx*dx)*2 + (dy*dy)*2 + (dz*dz)*2 
 }
 
 func parseInput(input [][]string) []Droplet {
@@ -34,12 +30,19 @@ func parseInput(input [][]string) []Droplet {
 		x, _ := strconv.Atoi(tokens[0])
 		y, _ := strconv.Atoi(tokens[1])
 		z, _ := strconv.Atoi(tokens[2])
+
+		minDroplet.x = util.Min(minDroplet.x, x)
+		minDroplet.y = util.Min(minDroplet.y, y)
+		minDroplet.z = util.Min(minDroplet.z, z)
+		maxDroplet.x = util.Max(maxDroplet.x, x)
+		maxDroplet.y = util.Max(maxDroplet.y, y)
+		maxDroplet.z = util.Max(maxDroplet.z, z)
 		droplets[i] = Droplet{x: x, y: y, z: z}
 	}
 	return droplets
 }
 
-func mapVerticiesPart1(droplets []Droplet) {
+func mapVerticies(droplets []Droplet) {
 	for _, d := range droplets {
 		// generate neighbour positions
 		neighbours := make([]Droplet, 6)
@@ -62,60 +65,68 @@ func mapVerticiesPart1(droplets []Droplet) {
 	}
 }
 
-func minMax(d Droplet, minMap, maxMap *DropletMap, val int) {
-	minD, minExists := (*minMap)[d]
-	maxD, maxExists := (*maxMap)[d]
-	if minExists {
-		(*minMap)[d] = util.Min(minD, val)
-	} else {
-		(*minMap)[d] = val
+func fill(d Droplet, lava *DropletMap, cube *DropletMap) bool {
+	_, exists := (*lava)[d]
+	if exists {
+		return false
 	}
-	if maxExists {
-		(*maxMap)[d] = util.Max(maxD, val)
-	} else {
-		(*maxMap)[d] = val
+	_, exists = (*cube)[d]
+	if exists {
+		return false
 	}
+	if d.x < minDroplet.x || d.x > maxDroplet.x {
+		return false
+	}
+	if d.y < minDroplet.y || d.y > maxDroplet.y {
+		return false
+	}
+	if d.z < minDroplet.z || d.z > maxDroplet.z {
+		return false
+	}
+	(*cube)[d] = 1
+	return true
 }
 
-func calcMinMax(droplets []Droplet) {
-	for _, d := range droplets {
-		// X
-		refD := Droplet{y: d.y, z: d.z}
-		minMax(refD, &minX, &maxX, d.x)
+func floodFill(minD, maxD Droplet, lava *DropletMap) int {
+	cubeDropletMap := make(DropletMap, 0)
+	searchStack := make([]Droplet, 1)
+	searchStack[0] = minD
 
-		// Y
-		refD = Droplet{x: d.x, z: d.z}
-		minMax(refD, &minY, &maxY, d.y)
+	for {
+		l := len(searchStack)
+		if l == 0 {
+			break
+		}
 
-		// X
-		refD = Droplet{x: d.x, y: d.y}
-		minMax(refD, &minZ, &maxZ, d.z)
-	}
-}
-
-func mapVerticiesPart2(droplets []Droplet) {
-	for _, d := range droplets {
-		// generate neighbour positions
-		neighbours := make([]Droplet, 6)
-		neighbours[0] = Droplet{x: d.x, y: d.y, z: d.z + 1}
-		neighbours[1] = Droplet{x: d.x, y: d.y, z: d.z - 1}
-		neighbours[2] = Droplet{x: d.x, y: d.y + 1, z: d.z}
-		neighbours[3] = Droplet{x: d.x, y: d.y - 1, z: d.z}
-		neighbours[4] = Droplet{x: d.x + 1, y: d.y, z: d.z}
-		neighbours[5] = Droplet{x: d.x - 1, y: d.y, z: d.z}
-
-		for _, n := range neighbours {
-			refD := Droplet{y: n.y, z: n.z}
-			xInside := n.x > minX[refD] && n.x < maxX[refD]
-			refD = Droplet{x: n.x, z: n.z}
-			yInside := n.y > minY[refD] && n.y < maxY[refD]
-			refD = Droplet{x: n.x, y: n.y}
-			zInside := n.z > minZ[refD] && n.z < maxZ[refD]
-			if xInside && yInside && zInside {
-				interiorDroplets[n] += 1
-			}
+		d := searchStack[l-1]
+		searchStack = searchStack[:l-1]
+		if fill(d, lava, &cubeDropletMap) {
+			neighbours := make([]Droplet, 6)
+			neighbours[0] = Droplet{x: d.x, y: d.y, z: d.z + 1}
+			neighbours[1] = Droplet{x: d.x, y: d.y, z: d.z - 1}
+			neighbours[2] = Droplet{x: d.x, y: d.y + 1, z: d.z}
+			neighbours[3] = Droplet{x: d.x, y: d.y - 1, z: d.z}
+			neighbours[4] = Droplet{x: d.x + 1, y: d.y, z: d.z}
+			neighbours[5] = Droplet{x: d.x - 1, y: d.y, z: d.z}
+			searchStack = append(neighbours, searchStack...)
 		}
 	}
+
+	cubeDroplets := make([]Droplet, len(cubeDropletMap))
+	verticies = make(DropletMap)
+
+	idx := 0
+	for d, _ := range cubeDropletMap {
+		cubeDroplets[idx] = d
+		idx++
+	}
+	mapVerticies(cubeDroplets)
+
+	area := 0
+	for _, v := range verticies {
+		area += v
+	}
+	return area - cubeArea(minD, maxD)
 }
 
 func Main(testmode bool) {
@@ -129,7 +140,7 @@ func Main(testmode bool) {
 	droplets := parseInput(input)
 	// fmt.Println(droplets)
 
-	mapVerticiesPart1(droplets)
+	mapVerticies(droplets)
 	fmt.Println(verticies)
 
 	// Part 1
@@ -140,26 +151,18 @@ func Main(testmode bool) {
 	fmt.Println(area)
 
 	// Part 2
-	// look over the vertices, and see which lie within the min/max of each x,y,z plane
-	// for those that do, reduce their value to 0 and re-sum the area
+	// start outside the lava and floodfill the space
+	// substrct exterior cube faces from total surface area of the
+	// mold generated
 	mapDroplets := make(DropletMap, len(droplets))
 	for _, d := range droplets {
 		mapDroplets[d] = verticies[d]
 	}
 
-	calcMinMax(droplets)
-	mapVerticiesPart2(droplets)
-	fmt.Println(interiorDroplets)
+	fmt.Println(minDroplet)
+	fmt.Println(maxDroplet)
 
-	interiorArea := 0
-	for _, v := range interiorDroplets {
-		interiorArea += v
-	}
-	fmt.Printf("Total area: %d\tExterior area: %d\n", area, interiorArea)
-	fmt.Println(minX)
-	fmt.Println(maxX)
-	fmt.Println(minY)
-	fmt.Println(maxY)
-	fmt.Println(minZ)
-	fmt.Println(maxZ)
+	interiorSurface := floodFill(minDroplet, maxDroplet, &mapDroplets)
+
+	fmt.Printf("Total area: %d\tExterior area: %d\n", area, interiorSurface)
 }
