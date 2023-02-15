93625fa64a9732c4d184c1025e42938ae36df5bc
Matt Brown <matthewpeterbrown@gmail.com>
2023-02-15 22:41:56 +0000

    Day 20 part 1 done

--- 2022/day20/day20.go
+++ 2022/day20/day20.go
@@ -29,23 +29,26 @@ func decode(orig []int) []int {
 
 	// build
 	// map values to list nodes for fast lookup
-	valueMap := make(map[int]*list.Element, len(orig))
+	size := len(orig)
+	origValues := make([]*list.Element, size)
 	buffer := list.New()
-	for _, n := range orig {
+	for i, n := range orig {
 		e := buffer.PushBack(n)
-		valueMap[n] = e
+		origValues[i] = e
 	}
 	printList(buffer)
 
 	// decode
-	for _, n := range orig {
+	for j := 0; j < size; j++ {
+		// lookup element to move
+		srcElem := origValues[j]
+		mark := srcElem
+
+		n := srcElem.Value.(int)
+		n = n % (size - 1)
 		if n == 0 {
 			continue
 		}
-
-		// lookup element to move
-		srcElem := valueMap[n]
-		mark := srcElem
 
 		// find insert point
 		for i := 0; i < util.Abs(n); i++ {
@@ -61,23 +64,27 @@ func decode(orig []int) []int {
 				}
 			}
 		}
-		if mark == buffer.Front() {
-			buffer.MoveAfter(srcElem, buffer.Back())
-		} else if mark == buffer.Back() {
-			buffer.MoveBefore(srcElem, buffer.Front())
-		} else if n > 0 {
-			buffer.MoveAfter(srcElem, mark)
+		if n > 0 {
+			if mark == buffer.Back() {
+				buffer.MoveBefore(srcElem, buffer.Front())
+			} else {
+				buffer.MoveAfter(srcElem, mark)
+			}
 		} else {
-			buffer.MoveBefore(srcElem, mark)
+			if mark == buffer.Front() {
+				buffer.MoveAfter(srcElem, buffer.Back())
+			} else {
+				buffer.MoveBefore(srcElem, mark)
+			}
 		}
 		printList(buffer)
 	}
 
 	// convert back to list
 	printList(buffer)
-	result := make([]int, len(orig))
+	result := make([]int, size)
 	l := buffer.Front()
-	for i := 0; i < len(orig); i++ {
+	for i := 0; i < size; i++ {
 		result[i] = l.Value.(int)
 		l = l.Next()
 	}
@@ -92,6 +99,7 @@ func calcResult(numbers []int) int {
 	for _, i := range idxs {
 		r := numbers[(startIdx+i)%size]
 		result += r
+		fmt.Printf("%dth value: %d\n", i, r)
 	}
 	return result
 }
